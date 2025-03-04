d9926a489ef3cbb3754572ef8694604ae790f267
Asabeneh <asabeneh@gmail.com>
2025-03-04 19:43:17 +0200

    week -3

---
+++ week-1/data-analysis-with-python.py
@@ -0,0 +1,58 @@
+'''
+Data Analysis with Python
+In this course, we will cover the following topics:
+1. NumPy
+2. Pandas
+3. TextBlob
+4. Matplotlib
+5. SciPy
+6. Scikitlearn
+7. Seaborn
+8. Statsmodels
+9. Scrapy
+10. Bokeh
+11. Plotly
+12. Scrapy
+13. BeautifulSoup
+14. Requests
+
+
+'''
+# with the following code:
+print('Hello World!')
+print('Wecome to Data Analysis with Python, 2025')
+
+'''
+NumPy: NumPy is the fundamental package for scientific computing with Python. It contains among other things a powerful N-dimensional array object, sophisticated (broadcasting) functions, tools for integrating C/C++ and Fortran code, useful linear algebra, Fourier transform, and random number capabilities.
+'''
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import seaborn as sns
+# import scipy
+# import statsmodels
+# import sklearn
+# import textblob
+# import requests
+
+# print(np.__version__)
+
+# ages = [23, 25, 22, 21, 23, 24, 28, 21, 24, 25, 27, 23, 24, 22, 23]
+# print(np.mean(ages))
+# print(np.median(ages))
+# print(np.std(ages))
+
+import webbrowser as wb
+wb.open_new_tab('https://www.numpy.org/')
+wb.open_new_tab('https://pandas.pydata.org/')
+wb.open_new_tab('https://textblob.readthedocs.io/en/dev/')
+wb.open_new_tab('https://matplotlib.org/')
+wb.open_new_tab('https://www.scipy.org/')
+wb.open_new_tab('https://scikit-learn.org/stable/')
+wb.open_new_tab('https://seaborn.pydata.org/')
+
+
+
+
+
+
---
+++ week-1/hello.py
@@ -0,0 +1,5 @@
+print('Hello World!')
+print('Wecome to Data Analysis with Python, 2025')
+print('Autosave is on, do not worry any more for saving')
+print(['NumPy','Pandas', 'TextBlob','Matplotlib', 'SciPy', 'Scikitlearn'])
+
